cada8d3b40255680acca6c9637abd470a5830e5f
Christian Panhuber <christianpanhuber@hotmail.com>
2019-09-01 12:26:55 +0200

    Add multiplication

---
+++ ned_ecef/include/ned_ecef/algebra/operations.h
@@ -0,0 +1,50 @@
+#ifndef NED_ECEF__ALGEBRA__OPERATIONS_H
+#define NED_ECEF__ALGEBRA__OPERATIONS_H
+
+#include <ned_ecef/algebra/types.h>
+
+#include <array>
+
+namespace ned_ecef
+{
+
+namespace algebra
+{
+
+template <size_t I, size_t J, size_t K, typename T, typename MatrixTypeLeft, typename MatrixTypeRight>
+Matrix<I, K, detail::MatrixImpl<I, K, T>, T> operator*(const Matrix<I, J, MatrixTypeLeft, T>& left,
+                                                       const Matrix<J, K, MatrixTypeRight, T>& right)
+{
+    auto result = MakeMatrix<I, K, T>();
+    for (size_t i = 0; i < I; ++i)
+    {
+        for (size_t k = 0; k < K; ++k)
+        {
+            result[i][k] = T{};
+            for (size_t j = 0; j < J; ++j)
+            {
+                result[i][k] += left[i][j] * right[j][k];
+            }
+        }
+    }
+
+    return result;
+}
+
+template <size_t N, typename T, typename MatrixTypeLeft, typename MatrixTypeRight>
+T operator*(const Matrix<N, 1, MatrixTypeLeft, T> left, const Matrix<1, N, MatrixTypeRight, T> right)
+{
+    T result{};
+    for (size_t i = 0; i < N; ++i)
+    {
+        result += left[0][i] * right[i][0];
+    }
+
+    return result;
+}
+
+}  // namespace algebra
+
+}  // namespace ned_ecef
+
+#endif  // NED_ECEF__ALGEBRA__OPERATIONS_H
---
+++ ned_ecef/include/ned_ecef/ned_ecef.h
@@ -0,0 +1,4 @@
+#ifndef NED_ECEF__NED_ECEF_H
+#define NED_ECEF__NED_ECEF_H
+
+#endif  // NED_ECEF__NED_ECEF_H
