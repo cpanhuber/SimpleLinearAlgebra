4297072321e4ecd18d5467294a7beb6d2520acf7
Christian Panhuber <christianpanhuber@hotmail.com>
2019-09-10 21:50:59 +0200

    Move self multiplication tests to types

--- silia/test/operations/scalar_multiplication_spec.cpp
+++ silia/test/operations/scalar_multiplication_spec.cpp
@@ -101,52 +101,6 @@ TEST(ScalarMultiplication, TransposedWithScalar)
     EXPECT_EQ(6, m2[1][1]);
     EXPECT_EQ(10, m2[1][2]);
 }
-
-TEST(ScalarMultiplicationAssignment, MatixWithScalarAssignment)
-{
-    auto m1 = MakeMatrix({{0, 1}, {2, 3}, {4, 5}});
-    int factor = 2;
-
-    m1 *= factor;
-
-    static_assert(std::is_same<decltype(m1), Matrix<3, 2, int>>::value);
-    EXPECT_EQ(0, m1[0][0]);
-    EXPECT_EQ(2, m1[0][1]);
-    EXPECT_EQ(4, m1[1][0]);
-    EXPECT_EQ(6, m1[1][1]);
-    EXPECT_EQ(8, m1[2][0]);
-    EXPECT_EQ(10, m1[2][1]);
-}
-
-TEST(ScalarMultiplicationAssignment, VectorWithScalarAssignment)
-{
-    auto v1 = MakeVector({0, 1, 2});
-    int factor = 2;
-
-    v1 *= factor;
-
-    static_assert(std::is_same<decltype(v1), Vector<3, int>>::value);
-    EXPECT_EQ(0, v1[0][0]);
-    EXPECT_EQ(2, v1[1][0]);
-    EXPECT_EQ(4, v1[2][0]);
-}
-
-TEST(ScalarMultiplicationAssignment, TransposedWithScalarAssignment)
-{
-    auto m1 = MakeMatrix({{0, 1}, {2, 3}, {4, 5}});
-    int factor = 2;
-
-    m1.TransposedView() *= factor;
-
-    static_assert(std::is_same<decltype(m1), Matrix<3, 2, int>>::value);
-    EXPECT_EQ(0, m1[0][0]);
-    EXPECT_EQ(2, m1[0][1]);
-    EXPECT_EQ(4, m1[1][0]);
-    EXPECT_EQ(6, m1[1][1]);
-    EXPECT_EQ(8, m1[2][0]);
-    EXPECT_EQ(10, m1[2][1]);
-}
-
 }  // namespace
 
 }  // namespace silia
