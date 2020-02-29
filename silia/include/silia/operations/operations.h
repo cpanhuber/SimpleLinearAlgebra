afa2357dfa581e8268f1fb5036fa7f1a5405b91e
Christian Panhuber <christianpanhuber@hotmail.com>
2020-02-29 23:27:39 +0100

    Add Addition Operator

--- silia/include/silia/operations/operations.h
+++ silia/include/silia/operations/operations.h
@@ -1,6 +1,7 @@
 #ifndef SILIA__OPERATIONS__OPERATIONS_H
 #define SILIA__OPERATIONS__OPERATIONS_H
 
+#include <silia/operations/addition.h>
 #include <silia/operations/multiplication.h>
 #include <silia/operations/scalar_multiplication.h>
 
